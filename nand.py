f9c3f3a0b6bb034a8fbda2f9902af7ade54e3100
David McFadzean <davidmc@gmail.com>
2024-05-29 21:16:35 -0400

    Refactored

--- nand.py
+++ nand.py
@@ -1,30 +1,28 @@
 import sys
 
-def next():
+def next_bit():
     bit = sys.stdin.read(1)
-
     if bit == '0':
         return 0
-
     if bit == '1':
         return 1
-
-    print()
-    exit(0)
+    raise EOFError
 
 def nand():
-    a = nand() if next() else next()
-    b = nand() if next() else next()
+    a = nand() if next_bit() else next_bit()
+    b = nand() if next_bit() else next_bit()
     return 1 if not (a and b) else 0
 
 def interpret():
-    while True:
-        n = next()
-
-        if n == 0:
-            print(next(), end="")
-        elif n == 1:
-            print(nand(), end="")
+    try:
+        while True:
+            n = next_bit()
+            if n == 0:
+                print(next_bit(), end="")
+            elif n == 1:
+                print(nand(), end="")
+    except EOFError:
+        print()
 
 if __name__ == "__main__":
     interpret()
