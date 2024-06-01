a63152dc1a339365d5f15983cd33560c716dd3a9
David McFadzean <davidmc@gmail.com>
2024-06-01 14:25:21 -0400

    Refactored

--- nand.py
+++ nand.py
@@ -1,32 +1,31 @@
 import sys
 from collections import deque
 
-bits = deque(sys.stdin.read().strip())
-
-def next_bit():
-    global bits
+def next_bit(bits):
+    if not bits:
+        raise IndexError("No more bits available.")
     bit = bits.popleft()
     if bit == '0':
         return 0
     if bit == '1':
         return 1
-    raise IndexError
+    raise ValueError("Invalid bit value: {}".format(bit))
 
-def nand():
-    return 1 if not (cond() and cond()) else 0
+def nand(bits):
+    return 1 if not (cond(bits) and cond(bits)) else 0
 
-def cond():
-    return nand() if next_bit() else next_bit()
+def cond(bits):
+    return nand(bits) if next_bit(bits) else next_bit(bits)
 
-def interpret():
-    global bits
+def interpret(bits):
     try:
-        while True:
-            bit = str(cond())
+        while bits:
+            bit = str(cond(bits))
             print(bit, end="")
             bits.append(bit)
-    except IndexError:
+    except (IndexError, ValueError) as e:
         print()
 
 if __name__ == "__main__":
-    interpret()
+    bits = deque(sys.stdin.read().strip())
+    interpret(bits)
